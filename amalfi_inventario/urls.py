e690a8b5fe2b38d1cb8631ea9cc94b4abab1e1f0
Victor <victorronquillohuanconjose@gmail.com>
2026-09-15 20:54:42 -0500

    Corregir contraste de texto muted en tema oscuro

--- amalfi_inventario/urls.py
+++ amalfi_inventario/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Amalfi - Sistema de Gestión de Inventarios"
+admin.site.site_title = "Amalfi Admin"
+admin.site.index_title = "Bienvenido al sistema de gestión de inventarios de Amalfi"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
@@ -9,4 +13,3 @@ urlpatterns = [
     path('', include('ubicaciones.urls')),
     path('', include('reportes.urls')),
 ]
-
